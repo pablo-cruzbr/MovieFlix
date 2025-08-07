11aea742992390a6879686f09f98d8cbf8678f29
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2025-08-07 17:49:27 -0300

    Exclusão da pagina Home
    não é necessário

--- src/router.jsx
+++ src/router.jsx
@@ -1,6 +1,5 @@
 import {BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Home from './pages/Home';
 import Filme from './pages/Filme';
 import Header from './components/Header';
 import Erro from './pages/Erro';
