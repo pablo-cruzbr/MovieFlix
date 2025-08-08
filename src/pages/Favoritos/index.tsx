18d5674aff425463d64b627a911637b230a53ed4
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2025-08-08 17:04:31 -0300

    Ajuste

--- src/pages/Favoritos/index.tsx
+++ src/pages/Favoritos/index.tsx
@@ -1,14 +1,47 @@
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import './favoritos.css';
 
-function Favoritos(){
-    return(
-        <div className="not-found">
-            <h1>Favoritos</h1>
-          
+
+interface Filme {
+  id: number;
+  title: string;
+
+}
+
+function Favoritos() {
+   
+    const [filmes, setFilmes] = useState<Filme[]>([]);
+
+    useEffect(() => {
+        const minhaLista = localStorage.getItem("@primeflix");
+        setFilmes(JSON.parse(minhaLista || '[]'));
+    }, []);
+
+    return (
+        <div className="meus-filmes">
+            <h1>Meus Filmes</h1>
+
+            <ul>
+                {filmes.length === 0 && <span>Você não tem nenhum filme salvo.</span>}
+
+                {filmes.map((item) => {
+                    return (
+                        <li key={item.id}>
+                            <span>{item.title}</span>
+
+                            <div>
+                                <Link to={`/detalhes/${item.id}`}> Ver detalhes</Link>
+                                <button>Excluir</button>
+                            </div>
+                        </li>
+                    );
+                })}
+            </ul>
+            
             <Link to="/">Veja todos Filmes!</Link>
         </div>
-    )
+    );
 }
 
 export default Favoritos;
--- src/router.jsx
+++ src/router.jsx
@@ -4,6 +4,7 @@ import Erro from './pages/Erro';
 import Filme from './pages/Filme';
 import Header from './components/Header';
 import Detalhes from './pages/Detalhes';
+import Favoritos from './pages/Favoritos';
 
 function RoutesApp() {
     return(
@@ -14,6 +15,7 @@ function RoutesApp() {
                 <Route path="/filme" element={<Filme/>}/>
                 <Route path='*' element={<Erro/>}/>
                 <Route path='/detalhes/:id' element={<Detalhes/>}/>
+                <Route path='/favoritos' element={<Favoritos/>}/>
             </Routes>
          
         </BrowserRouter>
