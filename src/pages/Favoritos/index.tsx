0cbb9b7975f2d4be762067af113579298727e781
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2025-08-11 11:11:25 -0300

    Exluindo e salvando atualizando filmes da lista

--- src/pages/Favoritos/index.tsx
+++ src/pages/Favoritos/index.tsx
@@ -2,46 +2,48 @@ import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import './favoritos.css';
 
-
 interface Filme {
   id: number;
   title: string;
-
 }
 
 function Favoritos() {
-   
-    const [filmes, setFilmes] = useState<Filme[]>([]);
+  const [filmes, setFilmes] = useState<Filme[]>([]);
 
-    useEffect(() => {
-        const minhaLista = localStorage.getItem("@primeflix");
-        setFilmes(JSON.parse(minhaLista || '[]'));
-    }, []);
+  function excluirItem(id: number) {
+    let filtroFilmes = filmes.filter((item) => {
+      return item.id !== id;
+    });
 
-    return (
-         <div className="movie-list-container"> {/* Alterado */}
-        <h1 className="movie-list-title">Meus Filmes</h1> {/* Alterado */}
+    setFilmes(filtroFilmes);
+    localStorage.setItem("@primeflix", JSON.stringify(filtroFilmes));
+  }
 
-        <ul className="movie-list"> {/* Alterado */}
-            {filmes.length === 0 && <span className="empty-movie-list-message">Você não tem nenhum filme salvo.</span>} {/* Alterado */}
+  useEffect(() => {
+    const minhaLista = localStorage.getItem("@primeflix");
+    setFilmes(JSON.parse(minhaLista || '[]'));
+  }, []);
 
-            {filmes.map((item) => {
-                return (
-                    <li key={item.id} className="movie-list-item"> {/* Alterado */}
-                        <span className="movie-title">{item.title}</span> {/* Alterado */}
-
-                        <div className="movie-actions"> {/* Alterado */}
-                            <Link to={`/detalhes/${item.id}`} className="movie-details-link"> Ver detalhes</Link> {/* Alterado */}
-                            <button className="movie-delete-button">Excluir</button> {/* Alterado */}
-                        </div>
-                    </li>
-                );
-            })}
-        </ul>
-
-        <Link to="/" className="view-all-link">Veja todos Filmes!</Link> {/* Alterado */}
+  return (
+    <div className="movie-list-container">
+      <h1 className="movie-list-title">Meus Filmes</h1>
+      <ul className="movie-list">
+        {filmes.length === 0 && <span className="empty-movie-list-message">Você não tem nenhum filme salvo.</span>}
+        {filmes.map((item) => {
+          return (
+            <li key={item.id} className="movie-list-item">
+              <span className="movie-title">{item.title}</span>
+              <div className="movie-actions">
+                <Link to={`/detalhes/${item.id}`} className="movie-details-link">Ver detalhes</Link>
+                <button className="movie-delete-button" onClick={() => excluirItem(item.id)}>Excluir</button>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+      <Link to="/" className="view-all-link">Veja todos Filmes!</Link>
     </div>
-    );
+  );
 }
 
 export default Favoritos;
